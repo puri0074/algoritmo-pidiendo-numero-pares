705153f944e30071fb4eb34cb9c03b8c0db050c7
Alumno <1472315@senati.pe>
2023-03-18 10:59:56 -0500

    ejercicio java

---
+++ SumandoNumeroPares.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sumando.numero.pares;
+
+/**
+ *
+ * @author Senati
+ */
+public class SumandoNumeroPares {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int sumaPares=0;
+        Scanner sc = new Scanner(System.in);
+        For (int i = o; i < 10; i++) {
+        
+            System.out.printf("ingrese los numeros pares (de 2 en 2)"): i + 2);
+            int numero = sc.nextInt();
+            
+    }
+    }
+    
+}
